b1a349c58d59c1371a635921bf6ec4f8133456eb
SomeRandomNerd <somerandomnerd@gmail.com>
2011-03-14 22:22:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myelectronicbrain::Application.config.secret_token = 'ce8d9fcb9edaa31d4a7380549f2b2efc087574b357f24d4e89bc5b2cfad7a57f6792556a031cc4247923a3a39ee4de6b81d7614bb3698271eea957d1b55e3d9e'
